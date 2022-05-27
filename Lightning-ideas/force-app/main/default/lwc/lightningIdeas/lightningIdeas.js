14a39a9f580a2e266c6b4503c8cba32b0b3b6f45
Kieran Elam <kieran.elam@experian.com>
2022-05-27 17:27:27 +0100

    2022-05-27

--- Lightning-ideas/force-app/main/default/lwc/lightningIdeas/lightningIdeas.js
+++ Lightning-ideas/force-app/main/default/lwc/lightningIdeas/lightningIdeas.js
@@ -2,36 +2,53 @@ import { LightningElement,wire } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getIdeas from '@salesforce/apex/ideasController.getIdeas';
 import createIdea from '@salesforce/apex/ideasController.createIdea';
+import deleteIdea from '@salesforce/apex/ideasController.deleteIdea';
 import voteOnIdea from '@salesforce/apex/ideasController.voteOnIdea';
 import addUpdateComment from '@salesforce/apex/ideasController.addUpdateComment';
 import deleteComment from '@salesforce/apex/ideasController.deleteComment';
 
 export default class LightningIdeas extends LightningElement {
 
-    showNewIdeaForm = true;
+    showNewIdeaForm = false;
+    error;
     newIdea = {};
 
     handleChange(event){
         this.newIdea[event.target.name] = event.target.value;
     }
 
-    newIdea(){
+    createNewIdea(){
+        console.log('in newIdea');
         this.showNewIdeaForm = true;
     }
 
-    createNewIdea() {
+    insertNewIdea() {
         //note can't use lightning-record-form/lightning/uiRecordApi as the object is not enabled for lightning!
-
-        createIdea({ newIdeaJSON: this.newIdea })
+        createIdea({ newIdeaJSON: JSON.stringify(this.newIdea) })
             .then((result) => {
-                this.contacts = result;
                 this.error = undefined;
                 this.showNewIdeaForm = false;
             })
             .catch((error) => {
+                console.log(JSON.stringify(error));
                 this.error = error;
-                this.contacts = undefined;
             });
+    }
+
+    deleteIdea(event){
+        deleteIdea({ideaId: event.currentTarget.dataset.id})
+            .then((result) => {
+                //remove from
+                this.ideas.remove(event.currentTarget.dataset.id);
+            })
+            .catch((error) => {
+                console.log(JSON.stringify(error));
+                this.error = error;
+            })
+    }
+
+    cancelNewIdea(){
+        this.showNewIdeaForm = false;
     }
 
     @wire(getIdeas)ideas;
